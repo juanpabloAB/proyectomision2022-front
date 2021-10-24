e8136d70036f012e303f6921c241bbc6dd210569
jsvanegasr <jsvanegasr@gmail.com>
2021-10-24 03:48:26 -0500

    Update CrearProducto.js
    Final Update

--- src/gestionProducto/pages/CrearProducto.js
+++ src/gestionProducto/pages/CrearProducto.js
@@ -1,144 +1,181 @@
-import * as React from 'react';
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import Table from '@mui/material/Table';
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import axios from "axios";
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import IconButton from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import {Box, Tooltip} from "@mui/material";
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import Typography from "@mui/material/Typography";
-import Checkbox from '@mui/material/Checkbox';
-import Toolbar from "@mui/material/Toolbar";
-import AddProduct from "./CrearProducto"
-import Popover from '@mui/material/Popover';
-import Button from '@mui/material/Button';
-import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-import Figure from 'react-bootstrap/Figure'
+import Modal from "@mui/material/Modal";
+import { TextField } from "@mui/material";
+import Backdrop from "@mui/material/Backdrop";
+import Fade from "@mui/material/Fade";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 
-function createData(id, title, description, price, stock, aviable, link) {
-    return { id, title, description, price, stock, aviable, link };
-  }
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
 
-const usefetchMore = (setProducts, url) => {
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  m: 2,
+  width: 1000,
+  height: 500,
+};
+
+
+export default function AddProduct(props) {
+  const [open, setOpen] = React.useState(false);
+  const [data, setData] = React.useState(props.edit?props.edit:{});
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const url = useSelector((state) => state.server.value);
+
+  const addProduct = (setOpen, url, data, handleUpdate) => {
     axios({
-      method: "GET",
-      url: `${url}/products`,
-    }).then((res) => {
-      const rows = [];
-      res.data.forEach((i) => {
-        rows.push(
-          createData(
-            i._id,
-            i.title,
-            i.description,
-            i.price,
-            i.stock,
-            i.aviable,  
-            i.link          
-          )
-        );
-      });
-      setProducts(rows);
-    });
-  };
+    method: "POST",
+    url: `${url}/products/new`,
+    data: data,
+  }).then((res) => {
+    setOpen(false);
+    handleUpdate(res);
+  });
+};
+const updateProduct = (setOpen, url, data, handleUpdate) => {
+  axios({
+    method: "PUT",
+    url: `${url}/products/edit`,
+    data: data,
+  }).then((res) => {
+    setOpen(false);
+    handleUpdate(res);
+  });
+};
 
-
-export default function BasicTable() {
-    const url = useSelector((state) => state.server.value);
-    const [products, setProducts] = useState([]);
-    const [open, setOpen] = React.useState(false);
-  
-    useEffect(() => {
-      usefetchMore(setProducts, url);
-     }, []);
-    
-
-  const handleUpdate = (ev) => {
-    usefetchMore(setProducts, url);
-  };
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.target);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const opened = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
-  return (
-    <TableContainer component={Paper}>
-    <Toolbar sx={{pl: { sm: 2 },pr: { xs: 1, sm: 1 },}}   
-          handleUpdate={handleUpdate}>
-        <Typography sx={{ flex: "1 1 100%" }} variant="h6" component="div">
-          Lista de Productos
-        </Typography>
-        <AddProduct handleUpdate={handleUpdate} />
-    </Toolbar>
-   
-      <Table sx={{ minWidth: 1000 }} aria-label="simple table">
-        <TableHead>
-          <TableRow >
-            <TableCell>ID</TableCell>
-            <TableCell align="center">Nombre</TableCell>
-            <TableCell align="center">Descripción</TableCell>
-            <TableCell align="center">Precio</TableCell>
-            <TableCell align="center">Stock</TableCell>
-            <TableCell align="center">Disponible</TableCell>
-            <TableCell align="center">Visualización</TableCell>
-            <TableCell align="center">Editar</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {products.map((row) => (
-              console.log(products),
-          <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
-              <TableCell component="th" scope="row">{row.id}</TableCell>  
-              <TableCell align="center">{row.title}</TableCell>
-              <TableCell align="center">{row.description}</TableCell>
-              <TableCell align="center">{row.price}</TableCell>
-              <TableCell align="center">{row.stock}</TableCell>
-              <TableCell align="center"><Checkbox checked = {row.aviable} /></TableCell>
-              <TableCell align="center">
-              <div>
-      <Button aria-describedby={id} variant="contained" onClick={handleClick}>
-      <RemoveRedEyeIcon />
-      </Button>
-      <Popover
-            id={id}
-            open={opened}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-            }}       
-      >                    <Figure>
-      <Figure.Image
-          width={513}
-          height={540}
-          alt="171x180"
-          src= {row.link}
-      />                    
-  </Figure>
-      </Popover>
-    </div>
-                </TableCell>
-                <TableCell> <AddProduct edit={row} handleUpdate={handleUpdate}/></TableCell>
-
-            </TableRow>
-          ))}
-        </TableBody>
+   return (
+    <div>  
+       {props.edit ? (
+        <Tooltip title="Edit">
+          <IconButton   className="btn-edt-product"
+                        onClick={handleOpen}
+                        variant="contained">
+                       
+            <ModeEditIcon />
+          </IconButton>
+          
+        </Tooltip>
+      ) : (
+        <IconButton
+          className="btn-add-product"
+          onClick={handleOpen}
+          variant="contained"
+        >
+          <AddIcon fontSize="large" />
+        </IconButton>
         
-      </Table>
-      
-    </TableContainer>
+      )}
+      <Modal
+        aria-labelledby="addproduct-modal-title"
+        aria-describedby="addproduct-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+        
+      >
+        
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="addproduct-modal-title" variant="h6" component="h2">
+              Crear nuevo producto
+            </Typography>
+            <Box
+              sx={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)" }}
+            >
+              <TextField
+                id="outlined"
+                label="Nombre producto"
+                size="small"
+                defaultValue={data.title}
+                sx={{ m: 2 }}
+                onChange={(e) => setData({ ...data, title: e.target.value })}
+              />
+              <TextField
+                id="outlined"
+                label="Descripción"
+                size="small"
+                sx={{ m: 2 }}
+                defaultValue={data.description}
+                onChange={(e) =>
+                  setData({ ...data, description: e.target.value })
+                }
+              />
+              <TextField
+                id="outlined"
+                label="Precio Unitario"
+                size="small"
+                defaultValue={data.price}
+                sx={{ m: 2 }}
+                onChange={(e) =>
+                  setData({ ...data, price: e.target.value })
+                }
+              />
+              <TextField
+                id="outlined"
+                label="Stock"
+                size="small"
+                sx={{ m: 2 }}
+                defaultValue={data.stock}
+                onChange={(e) => setData({ ...data, stock: e.target.value })}
+              />
+                <FormControl  size="small" sx={{ m: 2 }}>
+                    <InputLabel id="demo-simple-select-label">Disponible</InputLabel>
+                    <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={data.aviable}
+                    label="Disponible"
+                    onChange={(e) => setData({ ...data, aviable: e.target.value })}
+                    >
+                    <MenuItem value={"true"}>Disponible</MenuItem>
+                    <MenuItem value={"false"}>No Disponible</MenuItem>
+
+                    </Select>
+                </FormControl >
+              <TextField
+                id="outlined"
+                label="url"
+                size="small"
+                sx={{ m: 2 }}
+                defaultValue={data.link}
+                onChange={(e) => setData({ ...data, link: e.target.value })}
+              />         
+            <IconButton
+                sx={{ px:3} }
+                className="btn-add-product"
+                onClick={() => props.edit? updateProduct(setOpen, url, data, props.handleUpdate)
+                                          :addProduct(setOpen, url, data, props.handleUpdate)}
+                variant="contained"
+              >
+                <AddIcon fontSize="large" variant="contained" fullWidth/>
+                Confirmar
+                
+              </IconButton>
+                 </Box>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
   );
 }
-
